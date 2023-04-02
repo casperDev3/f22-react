2dd38352a3dd9c806457b79d43cecad90ebef38c
zerodott <93039953+casperDev3@users.noreply.github.com>
2023-04-02 11:45:07 +0300

    add lection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
 import "./App.css";
-// import components
+import React, { useState, useEffect } from "react";
+// components
 import First from "./components/first/First";
+import Post from "./components/posts/Post";
 
 function App() {
+  // reactive variables
+  const [posts, setPosts] = useState([]);
+  // on load
+  useEffect(() => {
+    getPosts();
+  }, []);
+  // functions
+  function getPosts() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        return setPosts(data);
+      });
+  }
+  function deletePost(id) {
+    const newPosts = posts.filter((post) => post.id !== id);
+    setPosts(newPosts);
+  }
   return (
     <div className="App">
       <First />
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+      >
+        {posts.map((post) => {
+          return <Post delete={deletePost} key={post.id} post={post} />;
+        })}
+      </div>
     </div>
   );
 }
--- src/components/first/First.jsx
+++ src/components/first/First.jsx
@@ -1,24 +1,26 @@
 import React from "react";
-// import custom modules
-import { Calculator } from "../../modules/Calculator";
+// custom modules
+// import { Calculator } from "../../modules/Calculator";
+// custom components
+import Number from "../numbers/Number";
 
 const First = () => {
-  const calc = new Calculator();
+  // const calc = new Calculator();
   const SOME_TEXT = "Hello World!";
   const arrayNumber = [1, 3, 2, 5];
-  function calcTwoNumbers(a, b) {
-    return a + b;
-  }
+  // function calcTwoNumbers(a, b) {
+  //   return a + b;
+  // }
   return (
     <>
       <h2>{SOME_TEXT}</h2>
       <div>
-        <p>{calcTwoNumbers(2, 4)}</p>
-        <p>{calc.sum(3, 2)}</p>
+        {/* <p>{calcTwoNumbers(2, 4)}</p>
+        <p>{calc.sum(3, 2)}</p> */}
       </div>
       <div>
         {arrayNumber.map((el) => {
-          return <p key={el}>{el}</p>;
+          return <Number number={el} key={el} />;
         })}
       </div>
       <h2>test</h2>
